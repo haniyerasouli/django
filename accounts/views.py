2f77792844caf9865cbdd61ab9f2abb078f4e1cf
unknown <haniyerasouli7@gmail.com>
2024-05-12 01:12:01 +0430

    third commit

--- accounts/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def ViewRegistration(request):
-    return render(request,'accounts/register_page.html')
--- shoes/admin.py
+++ shoes/admin.py
@@ -7,8 +7,18 @@ class KindAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':['name']}
     list_display = ['name']
 
+
+class ShoeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':['property','brand','color','price','gender']}
+    list_filter = ['price']
+    list_display = ['property','brand','color','price','gender']
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['range']
+
+
 # admin.site.register(Discription,DiscriptinAdmin)
-admin.site.register(Shoe)
+admin.site.register(Shoe,ShoeAdmin)
 admin.site.register(Discription)
 admin.site.register(Kind,KindAdmin)
-admin.site.register(Size)
+admin.site.register(Size,SizeAdmin)
